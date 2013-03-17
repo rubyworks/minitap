67e390d0a3125bb5c1896066c784522f6f474f5e
7rans <transfire@gmail.com>
2013-03-17 12:29:14 -0400

    Use global ignore callers variable.

--- lib/minitap/ignore_callers.rb
+++ lib/minitap/ignore_callers.rb
@@ -1,8 +1,5 @@
 ignore = /(lib|bin)#{Regexp.escape(File::SEPARATOR)}minitap/
 
-if defined?(RUBY_IGNORE_CALLERS)
-  RUBY_IGNORE_CALLERS << ignore
-else
-  RUBY_IGNORE_CALLERS = [ignore]
-end
+$RUBY_IGNORE_CALLERS = [] unless defined?($RUBY_IGNORE_CALLERS)
+$RUBY_IGNORE_CALLERS << ignore
 
