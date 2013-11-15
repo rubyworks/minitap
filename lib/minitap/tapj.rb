91188317e290a6708a8a891e10ca5cb07e0036d7
7rans <transfire@gmail.com>
2013-11-15 12:41:42 -0500

    Pure Minitest 5.

--- lib/minitap/tapj.rb
+++
@@ -1,5 +0,0 @@
-require 'minitap'
-MiniTest::Unit.runner = MiniTest::TapJ.new
-
-# TODO Support ENV['rpt'], but how to select other formats ?
-
--- lib/minitap/tapy.rb
+++
@@ -1,5 +0,0 @@
-require 'minitap'
-MiniTest::Unit.runner = MiniTest::TapY.new
-
-# TODO Support ENV['rpt'], but how to select other formats ?
-
